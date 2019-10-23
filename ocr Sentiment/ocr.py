428faad029929e537ea3637ffc3ee47233a6b8ff
suman shekhar <shekhar.shs@gmail.com>
2019-10-23 23:38:18 +0530

    Sentimal Analysis using OCR

---
+++ ocr Sentiment/ocr.py
@@ -0,0 +1,58 @@
+# import the necessary packages
+from PIL import Image
+import pytesseract
+import argparse
+import cv2
+import os
+import subprocess
+ 
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required=True,
+	help="path to input image to be OCR'd")
+ap.add_argument("-p", "--preprocess", type=str, default="thresh",
+	help="type of preprocessing to be done")
+args = vars(ap.parse_args())
+
+# load the example image and convert it to grayscale
+image = cv2.imread(args["image"])
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+ 
+# check to see if we should apply thresholding to preprocess the
+# image
+if args["preprocess"] == "thresh":
+	gray = cv2.threshold(gray, 0, 255,
+		cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+
+	# gray = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 115, 1)
+	
+ 
+# make a check to see if median blurring should be done to remove
+# noise
+# elif args["preprocess"] == "blur":
+# 	gray = cv2.medianBlur(gray, 3)
+ 
+# write the grayscale image to disk as a temporary file so we can
+# apply OCR to it
+filename = "{}.png".format(os.getpid())
+cv2.imwrite(filename, gray)
+
+
+# load the image as a PIL/Pillow image, apply OCR, and then delete
+# the temporary file
+text = pytesseract.image_to_string(Image.open(filename))
+os.remove(filename)
+
+f = open('ocrinput.txt','w')
+f.write(text)
+f.close()
+
+#
+#res_return = subprocess.check_output(["Rscript", "sentencesentiment.R", "ocrinput.txt", "result.txt"])
+#f = open('Sentimentresult.txt', 'w')
+#f.write(str(res_return))
+#f.close()
+
+# show the output images
+#cv2.imshow("Image", image)
+#cv2.imshow("Output", gray)
---
+++ ocr Sentiment/run.sh
@@ -0,0 +1,1 @@
+python ocr.py -i "index.png"
